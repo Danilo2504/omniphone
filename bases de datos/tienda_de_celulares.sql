1bf64b89a14e4301a84f2a60cda6f9dc14bcf342
marcelobuergo <buergomarcelo@gmail.com>
2021-09-14 22:55:49 -0300

    Actualizados nombres de tablas y agregados nuevos valores. Antiguas tablas en comentarios (temporal)

--- bases de datos/tienda_de_celulares.sql
+++ bases de datos/tienda_de_celulares.sql
@@ -1,11 +1,70 @@
-create table cliente
+create database tienda_de_celulares
+create table cliente(
+id_cliente int,
+contrasena varchar(50),
+dni int,
+domicilio varchar(50),
+cod_compra int unique,
+tel int,
+nom_user varchar (20),
+apell_user varchar (20),
+email varchar(50),
+perfil_img bigint,
+date_comp date,
+tarjeta int unique,
+CONSTRAINT PK_cliente PRIMARY KEY(id_cliente))
+
+create table compra(
+cod_compra int,
+met_pago money,
+fecha_compra date,
+id_compra int,
+CONSTRAINT PK_compra PRIMARY KEY(id_compra),
+-- CONSTRAINT FK_cliente_vent FOREIGN KEY(cod_compra) REFERENCES cliente(cod_compra)
+)
+
+create table producto
+(id_prod int,
+id_cliente int unique,
+color varchar(20),
+precio money,
+stock int,
+fot_prod bigint,
+nom_prod varchar(50),
+marca_prod varchar(20),
+envio int,
+CONSTRAINT PK_prod PRIMARY KEY(id_prod),
+constraint FK_prod_cliente FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente))
+
+CREATE TABLE caract(
+id_prod int,
+--id_cate (???)
+tamano varchar(40),
+memoria varchar(40),
+camara varchar(40),
+cpu varchar(40),
+antena varchar(40),
+ram smallint,
+cam_frtl smallint,
+procesador_ghz smallint,
+sist_opr varchar(20),
+rom varchar(20),
+bateria smallint,
+pantalla_res varchar(20),
+peso_gm smallint,
+lnch_date date,
+--constraint PK_caract primary key  (id_celular),
+--constraint FK_celular_caract FOREIGN KEY(id_celular) REFERENCES celular(id_celular) 
+)
+
+--codigo viejooooo--------------------------
+/*create table cliente 
 (cod_compra int unique,
 dni int,
 num_telefono int,
 nombre varchar (20),
 apellido varchar (20),
 date_comp date,
-
 CONSTRAINT PK_cliente PRIMARY KEY(dni))
 
 create table venta(
@@ -43,14 +102,14 @@ pantalla_res varchar(20),
 peso_gr smallint,
 lnch_date date
 constraint PK_caract primary key  (id_celular),
-constraint FK_celular_caract FOREIGN KEY(id_celular) REFERENCES celular(id_celular) 
-
+constraint FK_celular_caract FOREIGN KEY(id_celular) REFERENCES celular(id_celular) )
+*/
 /*drop table cliente
 drop table venta
 drop table caract
 drop table celular
 */
-INSERT INTO celular(id_celular,modelo,marca,cant_stock,precio,color) VALUES(2031,'Samsung Galaxy Core Prime','Samsung',40000000,3200,'Blanco')
+/*INSERT INTO celular(id_celular,modelo,marca,cant_stock,precio,color) VALUES(2031,'Samsung Galaxy Core Prime','Samsung',40000000,3200,'Blanco')
 INSERT INTO celular(id_celular,modelo,marca,cant_stock,precio,color) VALUES(2031,'Samsung Galaxy Core Prime','Samsung',40000000,3200,'Blanco')
 
 DROP PROCEDURE llamar_caract
@@ -59,4 +118,4 @@ CREATE PROCEDURE llamar_caract @id_celular int
 AS
 SELECT modelo FROM celular INNER JOIN caract ON celular.id_celular = caract.id_celular WHERE celular.id_celular=  @id_celular;
 GO
-exec llamar_caract @id_celular = 2031;
+exec llamar_caract @id_celular = 2031*/ 
