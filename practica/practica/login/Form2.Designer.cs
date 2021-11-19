6ea87cf1770e02c5696bcf241d39d4177c34e15a
seba galli <sebastiangalli20@gmail.com>
2021-11-19 14:23:20 -0300

    form para probar la conexion a sql NO FUNCIONA

--- practica/practica/login/Form2.Designer.cs
+++ practica/practica/login/Form2.Designer.cs
@@ -35,6 +35,7 @@ namespace login
             this.label_usuario = new System.Windows.Forms.Label();
             this.label_clave = new System.Windows.Forms.Label();
             this.btn_confirmar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // usuario
@@ -87,6 +88,15 @@ namespace login
             this.btn_confirmar.UseVisualStyleBackColor = false;
             this.btn_confirmar.Click += new System.EventHandler(this.btn_confirmar_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -94,6 +104,7 @@ namespace login
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(421, 276);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_confirmar);
             this.Controls.Add(this.label_clave);
             this.Controls.Add(this.label_usuario);
@@ -115,5 +126,6 @@ namespace login
         private System.Windows.Forms.Label label_usuario;
         private System.Windows.Forms.Label label_clave;
         private System.Windows.Forms.Button btn_confirmar;
+        private System.Windows.Forms.Button button1;
     }
 }
---
+++ pruebaBDD/pruebaBDD/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace pruebaBDD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Data Source = ALUMNO007\SQLEXPRESS;Initial Catalog = cuentas; User ID = ALUMNO007\Alumno;Password=";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                String str = "server=ALUMNO007\SQLEXPRESS;database=cuentas;UID=ALUMNO007\Alumno;integrated security=true";
+
+                String query = "select * from cuentas";
+
+                SqlConnection con = new SqlConnection(str);
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                con.Open();
+
+                DataSet ds = new DataSet();
+
+                MessageBox.Show("connect with sql server");
+
+                con.Close();
+
+            }
+
+            catch (Exception es)
+
+            {
+
+                MessageBox.Show(es.Message);
+
+
+            }
+        }
+}
