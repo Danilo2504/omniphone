cfcb0d5e193e71305002fe58d8dbc68d011120e3
Marcelo Buergo <buergomarcelo@gmail.com>
2021-12-02 17:57:38 -0300

    Prueba de login con SQL SERVER y C#.

--- bases de datos/cuentas.sql
+++ bases de datos/cuentas.sql
@@ -1,16 +1,18 @@
 create database cuentas
 use cuentas
 
-create table cuentas(
-id_cta int,
-mail varchar(50),
-nom_cta varchar(50),
-contrasena varchar(50)
+create table Cuenta(
+CuentaID int,
+MailCuenta varchar(50),
+NombreCuenta varchar(50),
+ContrasenaCuenta varchar(50)
 )
-
-insert into cuentas(id_cta,mail,nom_cta,contrasena) VALUES(0,'xd@gmail.com','saselandia','POTENCIA'),
+insert into Cuenta(CuentaID,MailCuenta,NombreCuenta,ContrasenaCuenta) VALUES(0,'xd@gmail.com','saselandia','POTENCIA'),
 (1,'mail@hotmail.com','potaxio','elpepe'),
 (2,'drno@gmail.com','yo y los papus','megapapu'),
 (3,'therat@rats.com','the rat that makes all of the rules','imakealltherules'),
 (4,'die@gmail.com','wayne','throws_dynamite')
+
+
+
 select * from cuentas
---
+++ pruebaBDD/loginBDD/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace loginBDD
+{
+    public partial class Form1 : Form
+    {
+        
+        String query = "select * from cuentas";
+        public static String str = @"server=CASA\SQLEXPRESS01;database=cuentas;UID=omniphone;integrated security=true";
+        SqlConnection con = new SqlConnection(str);
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            String queryCONTRA = "select NombreCuenta, ContrasenaCuenta from cuentas WHERE NombreCuenta ="+NombreCuenta.Text+"AND ContrasenaCuenta ="+ContraCuenta.Text;
+            SqlCommand cmd = new SqlCommand(queryCONTRA, con);
+            MessageBox.Show(cmd.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string querySELECT = "select NombreCuenta, ContrasenaCuenta from cuentas";
+            SqlCommand cmd = new SqlCommand(querySELECT, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}, {1}",
+                    reader["NombreCuenta"], reader["ContrasenaCuenta"]));// etc
+                }
+            }
+            finally
+            {
+                // Always call Close when done reading.
+                reader.Close();
+            }
+        }
+    }
+}
